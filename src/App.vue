6e92c8071b0d7b55a724d05c41fc32aaf4f9f598
henribru <6639509+henribru@users.noreply.github.com>
2021-09-15 23:07:26 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Foo :foo="false"/>
+</template>
+
+<script setup lang="ts">
+import Foo from '@/components/Foo.vue';
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Foo.vue
@@ -0,0 +1,21 @@
+ <template>
+ <div
+    :class="[
+      foo ? '' : bar(),
+      'foo',
+    ]"
+  >
+  Hello World
+</div>
+</template>
+
+<script setup lang="ts">
+import {
+  bar,
+} from "@/utils";
+
+
+defineProps({
+  foo: { type: Boolean, required: true },
+});
+</script>
